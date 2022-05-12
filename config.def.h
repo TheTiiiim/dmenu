b58c6f5145523c4ed2329fc13c23f24c2a1ed3ed
thetiiiim <0timothy.scott0@gmail.com>
2022-05-11 21:00:05 -0400

    remove pywal dependency

--- config.def.h
+++ config.def.h
@@ -10,7 +10,12 @@ static const char *fonts[] = {
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 
-#include "/home/thetiiiim/.cache/wal/colors-wal-dmenu.h"
+static char *colors[SchemeLast][2] = {
+	/*	fg	bg	*/
+	[SchemeNorm] = { "#bbbbbb", "#222222" },
+	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeOut] = { "#000000", "#00ffff" },
+};
 
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
